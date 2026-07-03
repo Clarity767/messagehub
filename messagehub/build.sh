2608af3d98794cd5eafd11621d44dff5f31d4c52
Глеб Шелег <shelehgleb@gmail.com>
2026-07-03 10:59:05 +0300

    fix requirements path in build.sh

--- messagehub/build.sh
+++ messagehub/build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-# Render запускає цей файл при кожному деплої
 set -o errexit
 
-pip install -r requirements.txt
+pip install -r ../requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
